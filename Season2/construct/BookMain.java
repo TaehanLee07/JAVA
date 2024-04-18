1ebbfda84fb718bdde25bafa0c85cb1b55e5b5b9
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-04-18 18:29:57 +0900

    Create BookMain.java

---
+++ Season2/construct/BookMain.java
@@ -0,0 +1,16 @@
+package construct;
+
+public class BookMain {
+
+    public static void main(String[] args) {
+        Book book1 = new Book();
+        book1.displayInfo();
+
+        Book book2 = new Book("Hello java", "SEO");
+        book2.displayInfo();
+
+        Book book3 = new Book("jpa 프로그래밍", "Kim" , 700);
+        book3.displayInfo();
+
+    }
+}
