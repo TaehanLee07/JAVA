8711ea450622f5937af0d8e4a0a3e4d16c990456
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-04-18 18:29:42 +0900

    Create Book.java

---
+++ Season2/construct/Book.java
@@ -0,0 +1,28 @@
+package construct;
+
+public class Book {
+    String title;
+    String author;
+    int page;
+
+    public Book() {
+        this.title = "";
+        this.author = "";
+    }
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.page = 0;
+    }
+    public Book(String title, String author, int page) {
+        this.title = title;
+        this.author = author;
+        this.page = page;
+    }
+    void displayInfo() {
+        System.out.println("제목 : " + this.title + " 저자 : " + " 페이지 : " + this.page);
+    }
+
+}
+
