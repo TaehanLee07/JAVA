d93c505c3c15f26c6712ccfd4e685c46b06f1265
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-04-19 15:55:30 +0900

    Create gumyengBoat.java

---
+++ Season2/greed/gumyengBoat.java
@@ -0,0 +1,31 @@
+package greedy;
+
+import java.util.Scanner;
+
+public class gumyengBoat {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int boats = sc.nextInt();
+		int peoplesUpper[] = new int[boats];
+		int peoplesLower[] = new int[boats];
+		
+		for (int i = 0; i < peoplesUpper.length; i++) {
+			peoplesUpper[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < peoplesLower.length; i++) {
+			peoplesLower[i] = sc.nextInt();
+
+		}
+		
+		for (int i = 0; i < peoplesUpper.length; i++) {
+			for (int j = 0; peoplesLower.length < 0; j--) {
+				
+			}
+		}
+	}
+	
+	
+	
+}
