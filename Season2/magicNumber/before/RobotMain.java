99f15ce494fd5c4b7c1188f0accbe8959bff24fa
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-05-07 16:54:38 +0900

    Create RobotMain.java

---
+++ Season2/magicNumber/before/RobotMain.java
@@ -0,0 +1,12 @@
+package magicNumber.before;
+
+public class RobotMain {
+
+    public static void main(String[] args) {
+       Robot robot = new Robot("Smith");
+       robot.order(0);
+       robot.order(1);
+       robot.order(2);
+       robot.order(3);
+    }
+}
