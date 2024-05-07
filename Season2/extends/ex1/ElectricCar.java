08e4814b4866b89b9a969372c79a3e482412366e
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-05-07 17:02:47 +0900

    Update ElectricCar.java

--- Season2/extends/ex1/ElectricCar.java
+++ Season2/extends/ex1/ElectricCar.java
@@ -1,1 +1,10 @@
+package extends1.ex1;
 
+public class ElectricCar {
+    public void move() { // 이동 기능
+        System.out.println("차를 이동합니다.");
+    }
+    public void charge() { // 충전 기능
+        System.out.println("충전합니다.");
+    }
+}
