548eb63762cfab7767175d193c28451cde29a595
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-05-07 17:02:58 +0900

    Update CarMain.java

--- Season2/extends/ex1/CarMain.java
+++ Season2/extends/ex1/CarMain.java
@@ -1,1 +1,16 @@
+package extends1.ex1;
+
+public class CarMain {
+    public static void main(String[] args) {
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move();
+        electricCar.charge();
+
+        System.out.println("==================");
+
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+        gasCar.fillUp();
+    }
+}
 
