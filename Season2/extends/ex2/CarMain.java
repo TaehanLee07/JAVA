9747267f7c0e87f0d4d71bd868b9d0de23407521
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-05-07 17:17:04 +0900

    Update CarMain.java

--- Season2/extends/ex2/CarMain.java
+++ Season2/extends/ex2/CarMain.java
@@ -7,6 +7,7 @@ public class CarMain {
         부모 클래스 (슈퍼 클래스) : 상속을 통해 자신의 필드 와 메서드를 다른 클래스에 제공하는 클래스 (Car.java)
         자식 클래스 (서브 클래스) : 부모 클래스로 부터 필드 와 메서드를 상속받는 클래스 (ElectricCar, GasCar)
         자바는 다중 상속을 지원하지 않는다 그래서 extends 대상은 하나만 선택할 수 있다. (단일 상속)
+        자바는 인터페이스를 이용해서 다중 구현을 사용하면 다중 상속과 같은 기능을 사용할 수 있다.
         */
 
         ElectricCar electricCar = new ElectricCar();
