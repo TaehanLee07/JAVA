c07901a179169598cef94fab5ff1df1df46d0c22
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-05-08 18:10:07 +0900

    Update Album.java

--- Season2/EXAM/exam01/Album.java
+++ Season2/EXAM/exam01/Album.java
@@ -3,12 +3,13 @@ package extends2;
 public class Album extends Item{
     String artist;
 
-    public Album(String name, int price, String artist) {
-        this.name = name;
-        price = getPrice();
+    public Album(String name, int price,String artist) {
+        super(name, price);
         this.artist = artist;
     }
+    @Override
     public void print() {
-        System.out.println("이름 : " + this.name + " 가격 : " + getPrice() + "\n-아티스트 : " + this.artist);
+        super.print();
+        System.out.println("아티스트 : " + artist);
     }
 }
