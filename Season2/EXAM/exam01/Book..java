99a14fe97650abb11b367f0ea7258c0a595dfda5
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-05-08 18:10:20 +0900

    Update Book..java

--- Season2/EXAM/exam01/Book..java
+++ Season2/EXAM/exam01/Book..java
@@ -1,16 +1,17 @@
 package extends2;
 
 public class Book extends Item{
-    String author;
-    String isbn;
+    private String author;
+    private String isbn;
 
     public Book(String name, int price,String author, String isbn) {
-        this.name = name;
-        price = getPrice();
+        super(name, price);
         this.author = author;
         this.isbn = isbn;
     }
+    @Override
     public void print() {
-        System.out.println("이름 : " + this.name + " 가격 : " + getPrice() + "\n-작가 : " + this.author + " isbn : " + this.isbn);
+        super.print();
+        System.out.println("저자 : " + author + "isbn : " + isbn);
     }
 }
