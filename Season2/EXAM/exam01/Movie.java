07e51714bb3cec8153aff2bbe56cf1ea95045406
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-05-08 18:10:46 +0900

    Update Movie.java

--- Season2/EXAM/exam01/Movie.java
+++ Season2/EXAM/exam01/Movie.java
@@ -4,13 +4,14 @@ public class Movie extends Item{
     String director;
     String actor;
 
-    public Movie(String name, int price, String director, String actor) {
-        this.name = name;
-        price = getPrice();
+    public Movie(String name, int price,String director, String actor) {
+        super(name, price);
         this.director = director;
         this.actor = actor;
     }
+    @Override
     public void print() {
-        System.out.println("이름 : " + this.name + " 가격 : " + getPrice() + "\n-감독 : " + this.director + " 배운 : " + this.actor);
+        super.print();
+        System.out.println("감독 : " + director + "배우 : " + actor);
     }
 }
