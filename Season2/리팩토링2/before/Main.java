fb053a321c0dabd5e34e1afe8a2c08a8ff2bd0fe
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-05-09 17:56:20 +0900

    Create Main.java

---
+++ Season2/리팩토링2/before/Main.java
@@ -0,0 +1,19 @@
+package removeControlflag.before;
+
+import java.io.*;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        SimpleDataBase simpleDataBase = new SimpleDataBase(new FileReader(".\\src\\datafile.txt"));
+
+        Iterator<String> ir = simpleDataBase.iterator();
+
+        while(ir.hasNext()) {
+            String key = ir.next();
+            System.out.println("key:" + key);
+            System.out.println("value: " + simpleDataBase.getValue(key));
+        }
+    }
+}
