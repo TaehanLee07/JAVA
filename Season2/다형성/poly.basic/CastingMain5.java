6162026020f612120ee2681c7aa8a25d8a13d0de
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-05-09 18:24:35 +0900

    Update CastingMain5.java

--- Season2/다형성/poly.basic/CastingMain5.java
+++ Season2/다형성/poly.basic/CastingMain5.java
@@ -22,10 +22,19 @@ public class CastingMain5 {
      지금처럼 다운캐스팅을 수행하기 전에 먼저 instanceof 를 사용해서 원하는 타입으로 변경이 가능한지 확인한 다음에 다운캐스팅을
      수행 하는것이 안전하다.
      참고로 instanceof 키워드는 오른쪽 대상의 자식 타입을 왼쪽에 참조하는 경우에도 true를 반환한다.
-     
+
      parent instanceof Parent : true
-     
+
      new Parent() instanceof Parent : true
      new Child() instanceof Parent : true
+     
+     new Parent() instanceof Parent : true
+     Parent p = new Parent() : true
+     
+     new Child() instanceof Parent : true
+     Parent p = new Child() : true
+     
+     new Parent() instanceof Child : false
+     Child c = new Parent() : false 자식은 부모를 품을 수 없다.
     */
 }
