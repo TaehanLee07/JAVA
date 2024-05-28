9ada66ce8cd0f376e89dd16b8f93468d09fe1896
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-05-28 18:09:38 +0900

    Create PayMain.java

---
+++ Season2/poly/ex/pay0/PayMain.java
@@ -0,0 +1,22 @@
+package poly.ex.pay0;
+
+public class PayMain {
+    public static void main(String[] args) {
+        PayService payService = new PayService();
+
+        // kakao 결제
+        String payOption = "kakao";
+        int amount = 100000;
+        payService.processPay(payOption, amount);
+
+        // naver 결제
+        String payOption2 = "naver";
+        int amount2 = 100000;
+        payService.processPay(payOption2,amount2);
+
+        // 잘못된 결제
+        String wrongOption = "apple";
+        int amount3 = 100000;
+        payService.processPay(wrongOption, amount3);
+    }
+}
