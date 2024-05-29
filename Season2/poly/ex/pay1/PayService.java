a38d9cb7fa61f6ca5c053a2a85769e154ff6e569
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-05-29 16:19:14 +0900

    Update PayService.java

--- Season2/poly/ex/pay1/PayService.java
+++ Season2/poly/ex/pay1/PayService.java
@@ -1,8 +1,18 @@
 package poly.ex.pay1;
 
-public class PayService implements Pay{
-    @Override
-    public void processPay(String option, int amount) {
+public class PayService {
+   public void processPay(String option, int amount) {
+       System.out.println("결제를 시작합니다 option : " + option + " amount : " + amount);
+       Pay pay = PayStore.findPay(option);
+       boolean result = pay.pay(amount);
 
-    }
+       if (result) {
+           System.out.println("결제가 성공했습니다.");
+       }else {
+           System.out.println("결제가 실패했습니다.");
+       }
+   }
 }
+
+
+
