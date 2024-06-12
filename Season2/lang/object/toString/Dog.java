70fdd866dc8541fb3e82c28d31b8f95a331983c3
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-12 16:34:38 +0900

    Create Dog.java

---
+++ Season2/lang/object/toString/Dog.java
@@ -0,0 +1,18 @@
+package lang.object.toString;
+
+public class Dog {
+    private String dogName;
+    private int age;
+
+    public Dog(String dogName, int age) {
+        this.dogName = dogName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "dogName='" + dogName + '\'' +
+                ", age=" + age + '}';
+    }
+}
