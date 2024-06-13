74eec2cfa778c6e6d06ed147d324e814139afb61
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-13 09:39:20 +0900

    Create 02_1.java

---
+++ Season2/학교수업/S21220/Package_00/02/02_1.java
@@ -0,0 +1,30 @@
+package S21220_00;
+
+public class S21220_02v1 {
+
+	public static void main(String[] args) {
+		
+		  int val1 =  Integer.parseInt("10");
+		  double val2 = Double.parseDouble("3.14");
+		  boolean val3 = Boolean.parseBoolean("true");
+		  
+		  System.out.println("val1 : " + val1);
+		  System.out.println("val2 : " + val2);
+		  System.out.println("val3 : " + val3);
+		  
+		  String str1 = String.valueOf(10);
+		  String str2 = String.valueOf(3.14);
+		  String str3 = String.valueOf(true);
+		  
+		  System.out.println("===========");
+
+		  System.out.println("str1 : " + str1);
+		  System.out.println("str2 : " + str2);
+		  System.out.println("str3 : " + str3);
+		  
+		  
+		  
+		  
+
+	}
+}
