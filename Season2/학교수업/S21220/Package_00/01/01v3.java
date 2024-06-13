f758ab20451bcbe29c995810069a84354b2220af
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-13 09:34:36 +0900

    Create 01v3.java

---
+++ Season2/학교수업/S21220/Package_00/01/01v3.java
@@ -0,0 +1,19 @@
+package S21220_00;
+
+public class S21220_01v3 {
+
+	public static void main(String[] args) {
+		
+		boolean stop = true;
+		//boolean stop = false;
+		int i = 10;
+		
+		
+		if (i > 9) {
+			System.out.println("운행 정지 합니다.");
+		}else {
+			System.out.println("운행 시작 합니다.");
+		}
+	}
+
+}
