0d54cd32cf82009b8ae439f63c9efa99477dbed0
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-13 09:39:08 +0900

    Create 02.java

---
+++ Season2/학교수업/S21220/Package_00/02/02.java
@@ -0,0 +1,20 @@
+package S21220_00;
+
+public class S21220_02 {
+
+	public static void main(String[] args) {
+		
+		  int value = 10 + 2 + 8;
+	      System.out.println("value : " + value);	
+	      
+	      String str1 = 10 + 2 + "8";
+	      String str2 = 100 + 2 + "8";
+	      String str3 = 10 + "28";
+	      String str4 = 100 + 1 + "0";	      
+	           
+	      System.out.println("str1 : " + str1);  	     
+	      System.out.println("str2 : " + str2);
+	      System.out.println("str3 : " + str3);
+	      System.out.println("str4 : " + str4);
+	}
+}
