984ca747748168c344977cbff81b290db68e782b
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-13 09:33:45 +0900

    Create 01.java

---
+++ Season2/학교수업/S21220/Package_00/01/01.java
@@ -0,0 +1,13 @@
+package S21220_00;
+
+public class S21220_01 {
+
+	public static void main(String[] args) {
+		int intNum1 = 0;
+		if(intNum1 == 0) {
+			int intNum2 = 10;
+			System.out.println("intNum1 + intNum2 = " + (intNum1 + intNum2));
+		}
+	}
+
+}
