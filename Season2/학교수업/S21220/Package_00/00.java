8791ea44b0d5fdd060c2e5d5a91761b185347ba1
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-13 09:31:43 +0900

    Create 00.java

---
+++ Season2/학교수업/S21220/Package_00/00.java
@@ -0,0 +1,14 @@
+package S21220_00;
+
+public class S21220_00 {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		
+		for (int i = 0; i < 11; i++) {
+			sum += i;
+		}
+		System.out.println("sum : " + sum);
+	}
+}
