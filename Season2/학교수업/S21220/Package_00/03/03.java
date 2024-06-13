3e0ed7fe3f32fa8d27970ca5a529f698883223c6
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-13 09:40:37 +0900

    Create 03.java

---
+++ Season2/학교수업/S21220/Package_00/03/03.java
@@ -0,0 +1,19 @@
+package S21220_00;
+
+public class S21220_03 {
+	public static void main(String[] args) {
+		
+		int input = 0;
+		
+		if (input == 0) 
+			System.out.println("a : 값은 0입니다");
+		if (input != 0)
+			System.out.println("a : 값은 0이 아닙니다");
+		
+		if (input == 0) 
+			System.out.println("b : 값은 0입니다");
+		else
+			System.out.println("b : 값은 0이 아닙니다");
+		
+	}
+}
