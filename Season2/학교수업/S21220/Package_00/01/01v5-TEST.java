2b0609a57e970357fcb68fe13d95591d20cb7332
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-13 09:35:30 +0900

    Create 01v5-TEST.java

---
+++ Season2/학교수업/S21220/Package_00/01/01v5-TEST.java
@@ -0,0 +1,12 @@
+package S21220_00;
+
+import java.io.IOException;
+
+public class S21220_01v5Test {
+	public static void main(String[] args) throws IOException  {
+		
+		System.out.print("키코드 입력 : \t");
+		int KeyCode = System.in.read();
+		System.out.println("keycode : " + KeyCode);
+	}
+}
