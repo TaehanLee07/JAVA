c836f703aef325fa1a78873cc8e56b1572e85c39
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-13 09:37:49 +0900

    Create 01v8.java

---
+++ Season2/학교수업/S21220/Package_00/01/01v8.java
@@ -0,0 +1,20 @@
+package S21220_00;
+
+public class S21220_01v8 {
+	public static void main(String[] args) {
+			
+			int intval1 = 100;
+			int intval2 = -100;
+			double dval1 = 3.14;
+			double dval2 = -3.14;
+			
+			int intres1 = - intval1;
+			int intres2 = - intval2;
+			double dres1 = - dval1;
+			double dres2 = - dval2;
+			
+			System.out.println("int1 : " + intres1 + " int2 : " + intres2);
+			System.out.println("dou1 : " + dres1 + " dou2 : " + dres2);
+
+	}
+}
