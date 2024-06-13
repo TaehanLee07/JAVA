fc4e1630de1901e470c21bea635caffacdd16d80
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-13 09:33:58 +0900

    Create 01v1.java

---
+++ Season2/학교수업/S21220/Package_00/01/01v1.java
@@ -0,0 +1,19 @@
+package S21220_00;
+
+public class S21220_01v1 {
+
+	public static void main(String[] args) {
+		int intNum1 = 0b1011;
+		int intNum2 = 0206;
+		int intNum3 = 365;
+		int intNum4 = 0xB3;
+		
+		System.out.println("Num 1 : " + intNum1);
+		System.out.println("Num 2 : " + intNum2);
+		System.out.println("Num 3 : " + intNum3);
+		System.out.println("Num 4 : " + intNum4);
+		
+		}
+	}
+
+
