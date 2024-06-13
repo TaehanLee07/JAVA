d41b46ab56bd667fa6bc2c5b67c8514c952a4ddd
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-13 09:30:08 +0900

    Create Sum.java

---
+++ Season2/학교수업/S21220/recursion/Sum.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class Sum {
+    static int sumPlus(int num) {
+        System.out.println("ing : " + num);
+        if (num == 1) {
+            return 1;
+        } else {
+            return num + sumPlus(num - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum : " + sumPlus(10));
+    }
+}
