72d14178b1b99fde3eb45a7e72cd30302d680c87
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-18 16:53:46 +0900

    Create StringBasicMain.java

---
+++ Season2/lang/string/StringBasicMain.java
@@ -0,0 +1,11 @@
+package lang.string;
+
+public class StringBasicMain {
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = new String("hello");
+
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
