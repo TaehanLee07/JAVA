3bb872f0d4367f8803dc5565c5a2644331fca0e2
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-18 10:28:15 +0900

    Create Temp_01_for2.java

---
+++ Season2/학교수업/S21220/Package_00/Test/Temp_01_for2.java
@@ -0,0 +1,18 @@
+package S21220_00;
+
+import java.io.IOException;
+
+public class Temp_01_for2 {
+	public static void main(String[] args) throws IOException {
+		int keycode;
+		for (;true;) {
+			keycode = System.in.read();
+			System.out.println("KeyCode :" + keycode);
+			
+			if (keycode == 113) {
+				break;
+			}
+		}
+		System.out.println("종료");
+	}
+}
