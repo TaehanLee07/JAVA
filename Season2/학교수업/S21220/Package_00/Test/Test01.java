e05e842df285729a71899d42f74f547adb849570
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-18 10:28:26 +0900

    Create Test01.java

---
+++ Season2/학교수업/S21220/Package_00/Test/Test01.java
@@ -0,0 +1,14 @@
+package S21220_00;
+
+public class Test01 {
+	public static void main(String[] args) {
+		int cnt = 1;
+		int sum = 0;
+		while(cnt <= 10) {
+			sum = sum + cnt;
+			cnt++;
+		}
+		System.out.println("sum : " + sum);
+	}
+	
+}
