29990f795672dc4f27153c3991be99d5205b1267
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-18 16:52:58 +0900

    Create CharArrayMain.java

---
+++ Season2/lang/string/CharArrayMain.java
@@ -0,0 +1,12 @@
+package lang.string;
+
+public class CharArrayMain {
+    public static void main(String[] args) {
+        // char 를 이용해서 hello 출력;
+        char c[] = {'H','e','l', 'l', 'o'};
+
+        System.out.println(c);
+        String s = "hello";
+        System.out.println(s);
+    }
+}
