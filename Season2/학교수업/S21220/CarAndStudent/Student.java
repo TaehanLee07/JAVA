54bb212bea5a3783ef01a7236c7fef0ea9f9f4c0
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-18 10:39:24 +0900

    Create Student.java

---
+++ Season2/학교수업/S21220/CarAndStudent/Student.java
@@ -0,0 +1,22 @@
+package S21220_java20;
+
+public class Student {
+	int a = 15;
+	
+	public void pub() {
+		System.out.println("public : " + a);
+	}
+	protected void pro() {
+		System.out.println("protected");
+	}
+	void def() {
+		System.out.println("default");
+	}
+	private void pri(int a, int b) {
+		int z = a + b;
+		System.out.println(z);
+	}
+	public void showPrivate(int a, int b) {
+		pri(a,b);
+	}
+}
