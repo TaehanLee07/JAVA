0a39cbdc09eaf938878dc3bcaefbe2d1a0cc90e9
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-18 10:29:25 +0900

    Create TestFormat.java

---
+++ Season2/학교수업/S21220/Package_00/Test/TestFormat.java
@@ -0,0 +1,11 @@
+package S21220_00;
+
+public class TestFormat {
+
+	public static void main(String[] args) {
+		  System.out.printf("이름 : %s", "우리들 ");
+		  System.out.printf("학번 : %d\n", 21200);
+		  System.out.printf("이름 : %s, 학번 : %d 전화번호 : %s \n", "우리들", 21200, "010-1234-5678");
+		  System.out.printf("이름 : %s, 전화번호 : %s 학번 : %d", "우리들", "010-1234-5678", 21200);
+	}
+}
