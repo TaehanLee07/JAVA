94f265d41a01dda0225f6610b6cb1491c79a6668
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-18 10:39:13 +0900

    Create StudentMain.java

---
+++ Season2/학교수업/S21220/CarAndStudent/StudentMain.java
@@ -0,0 +1,12 @@
+package S21220_java20;
+
+public class Chap01 {
+	public static void main(String[] args) {
+		Student student = new Student();
+		student.pub();
+		student.pro();
+		student.def();
+		
+		student.showPrivate(5, 10);
+	}
+}
