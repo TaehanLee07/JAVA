cfa2c4008f1480cc6fd18cff26f7604a3d5cc581
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-25 16:52:36 +0900

    Create WrapperClassMain.java

---
+++ Season2/lang/wrapper/WrapperClassMain.java
@@ -0,0 +1,10 @@
+package lang.wrapper;
+
+public class WrapperClassMain {
+    public static void main(String[] args) {
+        // Integer integer = new Integer(10);
+        Integer newInteger = 10;
+
+        Long newLong = 100l;
+    }
+}
