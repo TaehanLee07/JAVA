7f0d30bccde03e6ce1dec0d84710615e59c3671f
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-26 21:55:38 +0900

    Update Character.java

--- Season2/JavaRpgGame/characters/Character.java
+++ Season2/JavaRpgGame/characters/Character.java
@@ -1,4 +1,4 @@
-package charcaters;
+package characters;
 
 public class Character {
     public String name;
@@ -11,44 +11,45 @@ public class Character {
     public int totalMana;
     public int curMana;
     public int regenMana;
-    public int usedMana;
     public int shieldAmount = level;
-
 
     void normalAttack(String name, int attackDamage) {
         if (defenseLv >= attackDamage) {
             System.out.println("피해 상쇄됨.");
         } else {
-            System.out.println(name + "의 공격 "+ (attackDamage-defenseLv)+ "의 데미지를 입음..");
-            health -= attackDamage-defenseLv;
+            System.out.println(name + "의 공격 " + (attackDamage - defenseLv) + "의 데미지를 입음..");
+            health -= attackDamage - defenseLv;
         }
     }
-
-
 
     void passive() {
         // 캐릭터 별 추가예정
     }
 
     void firstSkill(Character character) {
-        if (usedMana >= curMana) {
+        if (curMana < usedMana) {
             System.out.println("마나가 부족하여 스킬을 사용할 수 없습니다.");
         }
     }
 
-    void secondSkill() {
+    void secondSkill(Character character) {
 
     }
 
-    void thirdSkill() {
+    void thirdSkill(Character character) {
 
     }
-
 
     void defense() {
         System.out.println("방어 사용");
-        health+=shieldAmount;
+        health += shieldAmount;
+    }
+
+    void invincibility() {
+        System.out.println("무적 상태가 발동되어 이번턴에는 죽지않습니다.");
+        if(this.health <= 0) {
+            this.health == 1;
+            System.out.println("무적 상태!");
+        }
     }
 }
-
-
