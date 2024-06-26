84301747bbff95669ab11ade7334f7303cf67da9
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-26 21:53:14 +0900

    Update Play.java

--- Season2/JavaRpgGame/game_system/Play.java
+++ Season2/JavaRpgGame/game_system/Play.java
@@ -1,18 +1,17 @@
 package game_system;
 
-import charcaters.Character;
+import characters.Character;
 
 public class Play {
     public int curTurn;
 
-
-    void turnStart() {
+    public void turnStart() {
         System.out.println(curTurn + "번째 턴 시작");
     }
 
-    void turnEnd() {
+    public void turnEnd(Character character) {
         System.out.println(curTurn + "턴 끝");
-        Character character = new Character();
         character.totalMana += character.regenMana;
+        System.out.println("캐릭터의 마나가 " + character.regenMana + "만큼 회복되었습니다.");
     }
 }
