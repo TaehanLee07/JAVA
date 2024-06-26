93ef59aeec39a3a25a5fac369c53dce893032ee0
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-06-26 21:53:03 +0900

    Update PlayMain.java

--- Season2/JavaRpgGame/playMain/PlayMain.java
+++ Season2/JavaRpgGame/playMain/PlayMain.java
@@ -1,5 +1,6 @@
-package gamePlayMain;
+package playMain;
 
+import characters.player.*;
 import java.util.Scanner;
 
 public class PlayMain {
@@ -8,17 +9,36 @@ public class PlayMain {
 
         while (true) {
             System.out.println("캐릭터를 생성합니다.\n원하는 직업군을 선택해 주세요\n" +
-                    "1. 전사 탭\t2. 탱커 탭\t3. 마법사 탭\t4. 암살자 탭\t5. 지원가 탭");
-            int choice = sc.nextInt();
-            if (choice == 1) {
-                System.out.println("전사탭을 고르셨습니다");
-                System.out.println("이중에서 골라 주세요 뒤로 가려면 0을 눌러주세요");
-                choice = sc.nextInt();
-                if (choice  == 0) {
-                    continue;
+                    "1. 전사 탭\t2. 탱커 탭\t3. 마법사 탭\t4. 암살자 탭\t5. 정령사 탭");
+
+            int num = sc.nextInt();
+
+            switch (num) {
+                case 1 -> {
+                    System.out.println("1. 바바리안\t2. 버서커");
+                    int num2 = sc.nextInt();
+                    switch (num2) {
+                        case 1 -> new Barbarian();
+                        case 2 -> new Berserker();
+                    }
+                }
+                case 2 -> {
+                    System.out.println("준비중");
+                    break;
+                }
+                case 3 -> {
+                    System.out.println("준비중");
+                    break;
+                }
+                case 4 -> {
+                    System.out.println("준비중");
+                    break;
+                }
+                case 5 -> {
+                    System.out.println("준비중");
+                    break;
                 }
             }
         }
     }
-    
 }
