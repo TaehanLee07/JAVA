4c5580a3bfd868984e77b532457d144301fdf9b4
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-10 17:38:07 +0900

    Create WrapperTest01.java

---
+++ Season2/lang/wrapper/test/WrapperTest01.java
@@ -0,0 +1,11 @@
+package lang.wrapper;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        String str = "10";
+        String str2 = "20";
+
+        // 문자로 입력된 str과 str2의 합을 작성
+        System.out.println(Integer.parseInt(str) + Integer.parseInt(str2));
+    }
+}
