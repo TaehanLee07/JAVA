84369ea0c4a3e34348a64a4693cf4ffb5af2f735
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-15 12:03:38 +0900

    Update Sql.sql

--- Season2/학교수업/p2024/p20240708/WebContent/Sql.sql
+++ Season2/학교수업/p2024/p20240708/WebContent/Sql.sql
@@ -1,1 +1,16 @@
 select max(custno)+1 from MEMBER_TBL_02;
+
+
+SELECT a.custno, a.custname, 
+       CASE a.grade 
+           WHEN 'A' THEN 'VIP' 
+           WHEN 'B' THEN '일반' 
+           ELSE '직원' 
+       END AS grade, 
+       SUM(b.price) AS price 
+FROM member_tbl_02 a, money_tbl_02 b
+WHERE a.custno = b.custno and NVL(b.price, 0) > 0 
+GROUP BY a.custno, a.custname, a.grade 
+ORDER BY SUM(b.price) DESC;
+
+select * from MONEY_TBL_02;
