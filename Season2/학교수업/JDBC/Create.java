2df9768daf0ce27b340ae5d737edef41cf827edf
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-17 09:31:33 +0900

    Update Create.java

--- Season2/학교수업/JDBC/Create.java
+++ Season2/학교수업/JDBC/Create.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Create {
 	public static void main(String[] args) throws Exception{
@@ -17,11 +18,31 @@ public class Create {
 		
 		// 3. Statement 객체 만들기
 		Statement st = con.createStatement();
-		
+
 		// 4. ResultSet 객체 만들기
-		ResultSet rs = st.executeQuery("select * from pc_emp");
+		ResultSet rs = st.executeQuery("select max(menu_no) from pc_menu");
 		
-		String sql = "insert into PC_menu values (10, '음료', '체리콕', 3000)";
+		rs.next();
+		
+		
+		int menu_no = rs.getInt(1)+1;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("메뉴 추가 페이지입니다. "+menu_no+"번째 ");
+				
+		System.out.printf("메뉴종류를 입력하세요. ");
+		String menu_type = sc.next();
+		
+		System.out.printf("메뉴명를 입력하세요. ");
+		String menu_name = sc.next();
+		
+		System.out.printf("메뉴가격를 입력하세요. ");
+		int menu_price = sc.nextInt();
+		
+		String sql = "insert into PC_menu values ("
+				+menu_no+", '"+
+				menu_type+"', '"+
+				menu_name+"', "+
+				menu_price+")";
 		
 		int res = st.executeUpdate(sql);
 		System.out.println(res);
