c4567a6a6c5ca9db73bdf9b25d872f79b5020aaa
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-17 09:31:53 +0900

    Create CreatePre.java

---
+++ Season2/학교수업/JDBC/CreatePre.java
@@ -0,0 +1,53 @@
+package jyt_jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class CreatePre {
+	public static void main(String[] args) throws Exception{
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		
+		// 1.드라이버 로드
+		Class.forName("oracle.jdbc.OracleDriver");
+		
+		// 2.Connection 객체 만들기
+		Connection con = DriverManager.getConnection(url,"sw212", "1234");
+		
+		// 3. Statement 객체 만들기
+		Statement st = con.createStatement();
+
+		// 4. ResultSet 객체 만들기
+		ResultSet rs = st.executeQuery("select max(menu_no) from pc_menu");
+		
+		rs.next();
+		int menu_no = rs.getInt(1)+1;
+
+		// ----- 메뉴번호 가져오는 과정
+		String sql = "insert into PC_menu values(?, ?, ?, ?)";
+		
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("메뉴 추가 페이지입니다. "+menu_no+"번째 ");
+		pstmt.setInt(1, menu_no);
+		
+		System.out.printf("메뉴종류를 입력하세요. ");
+		String menu_type = sc.next();
+		pstmt.setString(2, menu_type);
+		
+		System.out.printf("메뉴명를 입력하세요. ");
+		String menu_name = sc.next();
+		pstmt.setString(3, menu_name);
+		
+		System.out.printf("메뉴가격를 입력하세요. ");
+		int menu_price = sc.nextInt();
+		pstmt.setInt(4, menu_price); 
+		
+		int res = pstmt.executeUpdate();
+	}
+}
