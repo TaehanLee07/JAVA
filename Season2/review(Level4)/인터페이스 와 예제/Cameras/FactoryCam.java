151a645259d38a98bf4ebd47a600837a5ea53cca
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-23 16:55:55 +0900

    Create FactoryCam.java

---
+++ Season2/review(Level4)/인터페이스 와 예제/Cameras/FactoryCam.java
@@ -0,0 +1,30 @@
+package review.abs.camera;
+
+public class FactoryCam extends Camera implements Detectable, Reportable {
+
+    Detectable detector;
+    Reportable reporter;
+
+    @Override
+    public void showMainFeature() {
+        System.out.println("화재 감지");
+    }
+
+    @Override
+    public void detect() {
+        detector.detect();
+    }
+
+    @Override
+    public void report() {
+        reporter.report();
+    }
+
+    public void setDetector(Detectable detector) {
+        this.detector = detector;
+    }
+
+    public void setReporter(Reportable reporter) {
+        this.reporter = reporter;
+    }
+}
