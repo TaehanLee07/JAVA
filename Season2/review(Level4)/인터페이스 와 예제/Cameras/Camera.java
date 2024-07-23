980e9c841ee95ccda5b8b70e1c94961cd459e54c
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-23 16:56:14 +0900

    Update Camera.java

--- Season2/review(Level4)/인터페이스 와 예제/Cameras/Camera.java
+++ Season2/review(Level4)/인터페이스 와 예제/Cameras/Camera.java
@@ -1,11 +1,14 @@
 package review.abs.camera;
 
 public abstract class Camera {
+    
     public void takePicture() {
         System.out.println("사진을 촬영합니다.");
     }
+    
     public void recordViedo() {
         System.out.println("동영상을 녹화합니다.");
     }
+    
     public abstract void showMainFeature(); // 추상 메서드 - 하위 클래스에서 구현해야 하는 메서드
 }
