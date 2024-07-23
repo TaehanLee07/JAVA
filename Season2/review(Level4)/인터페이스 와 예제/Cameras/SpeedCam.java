b8f6b0691e8bd0c8f7082cb242b8a1d83c25f8ad
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-23 16:56:29 +0900

    Create SpeedCam.java

---
+++ Season2/review(Level4)/인터페이스 와 예제/Cameras/SpeedCam.java
@@ -0,0 +1,32 @@
+package review.abs.camera;
+
+public class SpeedCam extends Camera implements Detectable, Reportable{
+    Detectable detector;
+    Reportable reporter;
+
+    @Override
+    public void showMainFeature() {
+        System.out.println("속도를 측정하고 신고합니다.");
+    }
+
+    @Override
+    public void detect() {
+        detector.detect();
+    }
+
+    @Override
+    public void report() {
+        reporter.report();
+    }
+
+    public void setDetector(Detectable detector) {
+        System.out.println("감지기 객체 설정");
+        this.detector = detector;
+    }
+
+    public void setReporter(Reportable reporter) {
+        System.out.println("리포터 객체 설정");
+        this.reporter = reporter;
+    }
+
+}
