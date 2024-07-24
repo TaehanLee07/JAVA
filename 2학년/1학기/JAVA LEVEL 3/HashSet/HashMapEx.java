d954df67901c584fcb26d3cf3ce1337d83226eb0
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-24 21:40:18 +0900

    Create HashMapEx.java

---
+++ 2학년/1학기/JAVA LEVEL 3/HashSet/HashMapEx.java
@@ -0,0 +1,24 @@
+package hash;
+
+import java.util.HashMap;
+
+public class HashMapEx {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        hashMap.put("abc", 10);
+        hashMap.put("bbb", 20);
+        hashMap.put("aaa", 30);
+        hashMap.put("abc", 15); // 기존키의 value 인 10 대신 15가 들어간다
+
+        System.out.println(hashMap.isEmpty()); // false
+        System.out.println(hashMap.get("abc")); // 15
+        System.out.println(hashMap.containsKey("abc")); // true
+        hashMap.remove("abc");
+        System.out.println(hashMap.containsKey("abc")); // false
+        System.out.println(hashMap.size()); // 2
+        hashMap.clear();
+        System.out.println(hashMap.isEmpty()); // true
+        System.out.println(hashMap.size()); // 0
+    }
+}
