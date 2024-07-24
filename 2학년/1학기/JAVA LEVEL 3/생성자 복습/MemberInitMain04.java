da48176ed884ba023a9bc3640c5ef77fea913707
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-24 21:14:38 +0900

    Update MemberInitMain04.java

--- 2학년/1학기/JAVA LEVEL 3/생성자 복습/MemberInitMain04.java
+++ 2학년/1학기/JAVA LEVEL 3/생성자 복습/MemberInitMain04.java
@@ -5,10 +5,9 @@ public class MemberInitMain04 {
         MemberConstruct member1 = new MemberConstruct("user1",15,90);
         MemberConstruct member2 = new MemberConstruct("user2",16,80);
         MemberConstruct[]  members = {member1,member2};
-        for (MemberConstruct s : members ) {
-            System.out.println("이름 : " + s.name + " 나이 : " + s.age + " 성적 : " + s.grade);
+        for (MemberConstruct m : members ) {
+            System.out.println("이름 : " + m.name + " 나이 : " + m.age + " 성적 : " + m.grade);
         }
-
     }
 
 }
