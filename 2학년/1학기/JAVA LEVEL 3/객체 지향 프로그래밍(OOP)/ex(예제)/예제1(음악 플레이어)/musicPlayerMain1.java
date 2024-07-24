af63a8093aeedd1fa2b7e124fb41b19b7ab5c0ce
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-24 21:22:01 +0900

    Update musicPlayerMain1.java

--- 2학년/1학기/JAVA LEVEL 3/객체 지향 프로그래밍(OOP)/ex(예제)/예제1(음악 플레이어)/musicPlayerMain1.java
+++ 2학년/1학기/JAVA LEVEL 3/객체 지향 프로그래밍(OOP)/ex(예제)/예제1(음악 플레이어)/musicPlayerMain1.java
@@ -4,7 +4,7 @@ public class musicPlayerMain1 {
     public static void main(String[] args) {
         boolean isOn = false;
         int volume = 0;
-
+        
         isOn = true;
 
         System.out.println("플레이어 On");
@@ -19,17 +19,12 @@ public class musicPlayerMain1 {
         System.out.println("현재 볼륨 : "  + volume);
 
         // 음악플레이어 상태
-        if (isOn) {
+        if (isOn)
             System.out.println("음악플레이어 상태는 On 상태 볼륨은 : " + volume);
-        }else {
+        else 
             System.out.println("음악플레이어 상태는 Off");
-
-        }
 
         isOn = false;
         System.out.println("종료합니다");
-
     }
-
-
 }
