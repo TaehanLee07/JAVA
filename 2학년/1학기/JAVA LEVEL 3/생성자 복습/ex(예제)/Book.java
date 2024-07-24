2bf071653e83d1509b7a3f075e0b38b653ba5c7e
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-24 21:17:47 +0900

    Update Book.java

--- 2학년/1학기/JAVA LEVEL 3/생성자 복습/ex(예제)/Book.java
+++ 2학년/1학기/JAVA LEVEL 3/생성자 복습/ex(예제)/Book.java
@@ -15,11 +15,13 @@ public class Book {
         this.author = author;
         this.page = 0;
     }
+    
     public Book(String title, String author, int page) {
         this.title = title;
         this.author = author;
         this.page = page;
     }
+    
     void displayInfo() {
         System.out.println("제목 : " + this.title + " 저자 : " + " 페이지 : " + this.page);
     }
