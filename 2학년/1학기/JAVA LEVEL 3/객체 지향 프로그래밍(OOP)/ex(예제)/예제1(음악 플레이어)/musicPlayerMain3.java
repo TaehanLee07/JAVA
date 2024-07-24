945b3d1eb51dd8be73cfc8829557b4eff263767c
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-24 21:23:18 +0900

    Create musicPlayerMain3.java

---
+++ 2학년/1학기/JAVA LEVEL 3/객체 지향 프로그래밍(OOP)/ex(예제)/예제1(음악 플레이어)/musicPlayerMain3.java
@@ -0,0 +1,19 @@
+package oop;
+
+public class musicPlayerMain3 {
+    public static void main(String[] args) {
+
+        //켜기 on(date);
+        //볼륨 증가 volumeUp(data)
+        //볼륨 감소 volumeDown(data)
+        // 상태보기 showState
+        // 끄기 off (data)
+
+        musicPlayerData data = new musicPlayerData();
+        data.off();
+        data.on();
+        data.volUp();
+        data.volDown();
+        data.showState();
+    }
+}
