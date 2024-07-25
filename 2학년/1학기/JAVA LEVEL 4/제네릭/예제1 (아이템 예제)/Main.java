a2a57f28d3456d49628c1e762b7eec39b6760a13
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-07-25 15:04:15 +0900

    Create Main.java

---
+++ 2학년/1학기/JAVA LEVEL 4/제네릭/예제1 (아이템 예제)/Main.java
@@ -0,0 +1,11 @@
+package generics.ex1;
+
+public class Main {
+    public static void main(String[] args) {
+        Box<String, Integer> box1 = new Box<>("Apple", 10);
+        box1.display();
+
+        Box<Double, Character> box2 = new Box<>(3.14, 'A');
+        box2.display();
+    }
+}
