774574abc75af8281be58bace4d18a247cd82b86
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-09-04 17:33:27 +0900

    Create Student.java

---
+++ 2학년/2학기 복습/1일차 (클래스,, 인터페이스, 다형성 복습)/인터페이스 다형성 복습/Student.java
@@ -0,0 +1,69 @@
+package stu_teacherWith_Person;
+
+public class Student implements Person{
+    private String name;
+    private int grade;
+    private String studentId;
+    private Teacher t;
+
+    public Student(String name, int grade, String studentId) {
+        this.name = name;
+        this.grade = grade;
+        this.studentId = studentId;
+    }
+
+    // 교사 설정 getter & setter
+    public Teacher getT() {
+        return t;
+    }
+
+    public void setT(Teacher t) {
+        this.t = t;
+    }
+
+    // studentId 의 경우 값이 바뀌면 안되기에 getter 만 선언
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    // 나머지 필드값은 getter & setter 생성
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    // methods
+    public void study() {
+        System.out.println(name + " is Studying.");
+    }
+
+    // implements methods
+    @Override
+    public void showInfo() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Grade: " + grade);
+        System.out.println("StudentId: " + studentId);
+
+        if (t != null)
+            System.out.println("Teacher: " + t.getName());
+        else
+            System.out.println("No Teacher  Asigned.");
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
